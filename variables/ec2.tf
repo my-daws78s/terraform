f86ce71638690376d1b36d11c735422caa729b81
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-05-12 08:20:17 +0530

    terraform session2 started practice...

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,14 +1,37 @@
 # resource <resource-type> <resource-name>
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+resource "aws_instance" "db-instance" {
 
+  # left side before = are arguments and right side are called as values..
+  #ami = "ami-090252cbe067a9e58" #required field
+  ami = var.image_id
+  #instance_type = "t2.micro" #required field
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "db"
+    Purpose = "Murali's ec2 instance setup..."
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  #name = "allow_ssh"
+  name = var.sg_name
+  #description = "Allow TLS inbound traffic and all outbound traffic"
+  description = var.sg_description
+
+  # this is not map but block in terraform
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    #from_port        = 22
+    from_port         = var.ssh_port
+    #to_port           = 22
+    to_port           = var.ssh_port
+    #protocol         = "tcp"
+    protocol          = var.protocol
+    #cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks       = var.allowed_cidr
   }
 
   egress {
@@ -18,19 +41,10 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "allow_tls"
-    CreatedBy = "Murali Krishna"
-  }
-}
+  # tags = {
+  #   Name = "allow_tls"
+  #   CreatedBy = "Murali Krishna"
+  # }
 
-resource "aws_instance" "db-instance" {
-  ami           = "ami-090252cbe067a9e58"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "db"
-    Purpose = "Murali's ec2 instance setup..."
-  }
+  tags = var.tags
 }
--- variables/variables.tf
+++ variables/variables.tf
@@ -1,4 +1,43 @@
-variable "image_id" { # picked from google variables in terraform
-    
+#ami           = "ami-090252cbe067a9e58"
+#vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#instance_type = "t2.micro"
 
+# we now try to define all values from ec2.tf inside variables..
+
+variable "image_id" { # picked from google variables in terraform
+    type = string #optional
+    default = "ami-090252cbe067a9e58" #optional
+    description = "RHEL-9-DevOps-Practice" #optional
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+    type = string
+}
+
+variable "tags" {
+    default = {
+        Project = "Expense"
+        Environment = "DEV"
+        Module = "DB"
+        Name = "DB"
+    }
+}
+
+variable "sg_name" {
+    default = "allow_ssh"
+}
+
+variable "sg_description" {
+    default = "Allowing port 22"
+}
+
+variable "ssh_port" {
+    default = 22
+}
+variable "protocol" {
+    default = "tcp"
+}
+variable "allowed_cidr" {
+    default = ["0.0.0.0/0"]
 }
